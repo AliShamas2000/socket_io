aa7173cc066c2f2467a1f4f95df85a061e1d5558
AliShamasWebAddicts <ali.shamas@thewebaddicts.com>
2024-10-28 15:26:53 +0200

    Commit changes

--- index.js
+++ index.js
@@ -8,33 +8,47 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Replace with your specific origin for production
     methods: ["GET", "POST"],
   },
 });
 
 const registers = {};
+let connectedClients = 0; // Variable to keep track of connected clients
 
 app.use(express.json());
 
 io.on("connection", (socket) => {
+  connectedClients++;
+  console.log(`New client connected: ${socket.id}`);
+  console.log(`Total connected clients: ${connectedClients}`);
+
   socket.on("register", (identifier) => {
     registers[identifier] = socket.id;
+    console.log(`User registered: ${identifier} with socket ID: ${socket.id}`);
   });
 
   socket.on("message", (data) => {
     const targetTo = data.to;
-    const payload = data.payload; 
-    if(registers[targetTo]){
-        io.to(registers[targetTo]).emit('message' , payload);
+    const payload = data.payload;
+    if (registers[targetTo]) {
+      io.to(registers[targetTo]).emit("message", payload);
+      console.log(`Message sent to ${targetTo}`);
+    } else {
+      console.log(`User with identifier ${targetTo} is not connected`);
     }
   });
 
   socket.on("disconnect", () => {
-    for(const [userId , socketId] of Object.entries(registers)){
-        if(socketId === socket.id){
-            delete registers[userId];
-        }
+    connectedClients--;
+    console.log(`Client disconnected: ${socket.id}`);
+    console.log(`Total connected clients: ${connectedClients}`);
+
+    for (const [userId, socketId] of Object.entries(registers)) {
+      if (socketId === socket.id) {
+        console.log(`User disconnected: ${userId}`);
+        delete registers[userId];
+      }
     }
   });
 });
